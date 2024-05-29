0fbdc307df96d224dd9c19ee62a7c19e65d11e98
Jakub Krispel <jkrispel@st.swps.edu.pl>
2024-05-29 21:40:48 +0200

    widok dodawanie produktu wszystkie progress bary dzialaja, rowniez kalorie (dzieki pomocy gpt4o)

--- app/src/main/java/pl/edu/swps/sofitthisweekv2/Nutrition.kt
+++ app/src/main/java/pl/edu/swps/sofitthisweekv2/Nutrition.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.min
 import kotlin.math.roundToInt
 
 class Nutrition : AppCompatActivity() {
@@ -30,94 +31,11 @@ class Nutrition : AppCompatActivity() {
     private lateinit var barFats: ProgressBar
     private lateinit var barCarbs: ProgressBar
 
-
-    private fun loadGoalData() {
-        val sharedPreferences = getSharedPreferences("UserGoal", Context.MODE_PRIVATE)
-        val calories = sharedPreferences.getString("Calories", "")
-        val protein = sharedPreferences.getString("Protein", "")
-        val fats = sharedPreferences.getString("Fats", "")
-        val carbs = sharedPreferences.getString("Carbs", "")
-        val caloriesValue = calories?.toIntOrNull() ?: 1
-
-        caloriesMax.text = "/${caloriesValue}"
-        proteinMax.text = run {
-            val proteinValue = protein?.toIntOrNull() ?: 1
-            "/${(proteinValue.toFloat() * caloriesValue / 400).roundToInt()}g"
-        }
-        fatsMax.text = run {
-            val fatsValue = fats?.toIntOrNull() ?: 1
-            "/${(fatsValue.toFloat() * caloriesValue / 900).roundToInt()}g"
-        }
-        carbsMax.text = run {
-            val carbsValue = carbs?.toIntOrNull() ?: 1
-            "/${(carbsValue.toFloat() * caloriesValue / 400).roundToInt()}g"
-        }
-    }
-
-
-    private fun loadNewProduct() {
-
-        // Get the Intent extras
-        val extras = intent.extras
-        if (extras != null) {
-            val newCalories = extras.getInt("calories", 0)
-            val newProtein = extras.getInt("protein", 0)
-            val newFats = extras.getInt("fats", 0)
-            val newCarbs = extras.getInt("carbs", 0)
-
-            // Get existing values from TextViews
-            val currentCalories = caloriesCount.text.toString().toIntOrNull() ?: 0
-            val currentProtein = proteinCount.text.toString().toIntOrNull() ?: 0
-            val currentFats = fatsCount.text.toString().toIntOrNull() ?: 0
-            val currentCarbs = carbsCount.text.toString().toIntOrNull() ?: 0
-
-            // Calculate new totals
-            val totalCalories = currentCalories + newCalories
-            val totalProtein = currentProtein + newProtein
-            val totalFats = currentFats + newFats
-            val totalCarbs = currentCarbs + newCarbs
-
-            // Update TextView elements with new totals
-            caloriesCount.text = totalCalories.toString()
-            proteinCount.text = totalProtein.toString()
-            fatsCount.text = totalFats.toString()
-            carbsCount.text = totalCarbs.toString()
-        }
-    }
-
-
-    private fun saveCurrentMacro() {
-        val sharedPreferences = getSharedPreferences("UserMacro", Context.MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-        editor.putString("Calories", caloriesCount.text.toString())
-        editor.putString("Protein", proteinCount.text.toString())
-        editor.putString("Fats", fatsCount.text.toString())
-        editor.putString("Carbs", carbsCount.text.toString())
-        editor.apply() // or editor.commit() for synchronous saving
-
-
-    }
-
-
-    private fun loadCurrentMacro() {
-
-        val sharedPreferences = getSharedPreferences("UserMacro", Context.MODE_PRIVATE)
-        val calories = sharedPreferences.getString("Calories", "")
-        val protein = sharedPreferences.getString("Protein", "")
-        val fats = sharedPreferences.getString("Fats", "")
-        val carbs = sharedPreferences.getString("Carbs", "")
-
-        caloriesCount.text = calories
-        proteinCount.text = protein
-        fatsCount.text = fats
-        carbsCount.text = carbs
-    }
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nutrition)
 
+        // Initialize UI elements
         caloriesMax = findViewById(R.id.caloriesMax)
         proteinMax = findViewById(R.id.proteinMax)
         fatsMax = findViewById(R.id.fatsMax)
@@ -135,20 +53,17 @@ class Nutrition : AppCompatActivity() {
         loadCurrentMacro()
         loadNewProduct()    // 0 values if null
         saveCurrentMacro()
-        // TODO UPDATE THE PROGRESS BARS
         updateProgressBars()
 
-
+        // Set button listeners
         bAddProduct = findViewById(R.id.bAddProduct)
         bAddProduct.setOnClickListener {
-
             val intent = Intent(this, AddProduct::class.java)
             startActivity(intent)
         }
 
         bChangeGoal = findViewById(R.id.bChangeGoal)
         bChangeGoal.setOnClickListener {
-
             val intent = Intent(this, NutritionChangeGoal::class.java)
             startActivity(intent)
         }
@@ -178,8 +93,74 @@ class Nutrition : AppCompatActivity() {
         }
     }
 
+    private fun loadGoalData() {
+        val sharedPreferences = getSharedPreferences("UserGoal", Context.MODE_PRIVATE)
+        val calories = sharedPreferences.getString("Calories", "")
+        val protein = sharedPreferences.getString("Protein", "")
+        val fats = sharedPreferences.getString("Fats", "")
+        val carbs = sharedPreferences.getString("Carbs", "")
+        val caloriesValue = calories?.toIntOrNull() ?: 1
+
+        caloriesMax.text = "/${caloriesValue}"
+        proteinMax.text = run {
+            val proteinValue = protein?.toIntOrNull() ?: 1
+            "/${(proteinValue.toFloat() * caloriesValue / 400).roundToInt()}g"
+        }
+        fatsMax.text = run {
+            val fatsValue = fats?.toIntOrNull() ?: 1
+            "/${(fatsValue.toFloat() * caloriesValue / 900).roundToInt()}g"
+        }
+        carbsMax.text = run {
+            val carbsValue = carbs?.toIntOrNull() ?: 1
+            "/${(carbsValue.toFloat() * caloriesValue / 400).roundToInt()}g"
+        }
+    }
+
+    private fun loadNewProduct() {
+        val extras = intent.extras
+        if (extras != null) {
+            val newCalories = extras.getInt("calories", 0)
+            val newProtein = extras.getInt("protein", 0)
+            val newFats = extras.getInt("fats", 0)
+            val newCarbs = extras.getInt("carbs", 0)
+
+            val currentCalories = caloriesCount.text.toString().toIntOrNull() ?: 0
+            val currentProtein = proteinCount.text.toString().toIntOrNull() ?: 0
+            val currentFats = fatsCount.text.toString().toIntOrNull() ?: 0
+            val currentCarbs = carbsCount.text.toString().toIntOrNull() ?: 0
+
+            caloriesCount.text = (currentCalories + newCalories).toString()
+            proteinCount.text = (currentProtein + newProtein).toString()
+            fatsCount.text = (currentFats + newFats).toString()
+            carbsCount.text = (currentCarbs + newCarbs).toString()
+        }
+    }
+
+    private fun saveCurrentMacro() {
+        val sharedPreferences = getSharedPreferences("UserMacro", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString("Calories", caloriesCount.text.toString())
+        editor.putString("Protein", proteinCount.text.toString())
+        editor.putString("Fats", fatsCount.text.toString())
+        editor.putString("Carbs", carbsCount.text.toString())
+        editor.apply()
+    }
+
+    private fun loadCurrentMacro() {
+        val sharedPreferences = getSharedPreferences("UserMacro", Context.MODE_PRIVATE)
+        val calories = sharedPreferences.getString("Calories", "")
+        val protein = sharedPreferences.getString("Protein", "")
+        val fats = sharedPreferences.getString("Fats", "")
+        val carbs = sharedPreferences.getString("Carbs", "")
+
+        caloriesCount.text = calories
+        proteinCount.text = protein
+        fatsCount.text = fats
+        carbsCount.text = carbs
+    }
+
     private fun updateProgressBars() {
-        val maxCalories = caloriesMax.text.toString().toIntOrNull() ?: 1
+        val maxCalories = caloriesMax.text.toString().filter { it.isDigit() }.toIntOrNull() ?: 1
         val maxProtein = proteinMax.text.toString().filter { it.isDigit() }.toIntOrNull() ?: 1
         val maxFats = fatsMax.text.toString().filter { it.isDigit() }.toIntOrNull() ?: 1
         val maxCarbs = carbsMax.text.toString().filter { it.isDigit() }.toIntOrNull() ?: 1
@@ -194,9 +175,9 @@ class Nutrition : AppCompatActivity() {
         barFats.max = maxFats
         barCarbs.max = maxCarbs
 
-        barCalories.progress = minOf(currentCalories, maxCalories)
-        barProtein.progress = minOf(currentProtein, maxProtein)
-        barFats.progress = minOf(currentFats, maxFats)
-        barCarbs.progress = minOf(currentCarbs, maxCarbs)
+        barCalories.progress = min(currentCalories, maxCalories)
+        barProtein.progress = min(currentProtein, maxProtein)
+        barFats.progress = min(currentFats, maxFats)
+        barCarbs.progress = min(currentCarbs, maxCarbs)
     }
 }
